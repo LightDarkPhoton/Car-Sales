cbb045e68b85256272e201bfb2b9a177e7349e70
Andrew Ruiz <nukedoom@gmail.com>
2020-10-18 17:35:57 -0700

    Completed project

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+// Step 1: Create Store
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import { reducer } from './reducers/reducer';
+
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+// Step 2: Wrap Provider around App and pass store via props in Provider
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, rootElement);
